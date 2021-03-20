420d808c04a485b45eb1ccc336e980cf996677e9
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-03-20 18:20:04 +0100

    Possibility of removing components

--- pyengine/main_window/widgets/components_widget.py
+++ pyengine/main_window/widgets/components_widget.py
@@ -14,13 +14,17 @@ class ComponentsWidget(QWidget):
         self.obj = None
 
         self.title = QLabel("None", self)
-        self.title.setFont(QFont("arial", 18, 1, False))
-        self.title.setAlignment(Qt.AlignHCenter)
         self.list_components = QListWidget(self)
         self.add_component = QPushButton("Add Component", self)
+
+        self.title.setFont(QFont("arial", 18, 1, False))
+        self.title.setAlignment(Qt.AlignHCenter)
+        self.list_components.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.list_components.setSelectionMode(QAbstractItemView.SingleSelection)
         self.add_component.setEnabled(False)
 
         self.add_component.clicked.connect(self.add)
+        self.list_components.customContextMenuRequested.connect(self.components_context_menu)
 
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.title)
@@ -29,6 +33,24 @@ class ComponentsWidget(QWidget):
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.layout.setSpacing(0)
         self.setLayout(self.layout)
+
+    def components_context_menu(self):
+        menu = QMenu(self)
+        create_transform = QAction("Transform", self)
+        create_transform.triggered.connect(lambda: self.create_component("TransformComponent"))
+        menu.addAction(create_transform)
+        create_sprite = QAction("Sprite", self)
+        create_sprite.triggered.connect(lambda: self.create_component("SpriteComponent"))
+        menu.addAction(create_sprite)
+        create_script = QAction("Script", self)
+        create_script.triggered.connect(lambda: self.create_component("ScriptComponent"))
+        menu.addAction(create_script)
+        if len(self.list_components.selectedItems()) >= 1:
+            widget = self.list_components.itemWidget(self.list_components.selectedItems()[0])
+            remove = QAction("Delete Component", self)
+            remove.triggered.connect(lambda: self.remove_component(widget.component.name))
+            menu.addAction(remove)
+        menu.exec_(QCursor.pos())
 
     def add(self):
         menu = QMenu(self)
@@ -39,7 +61,13 @@ class ComponentsWidget(QWidget):
         create_sprite.triggered.connect(lambda: self.create_component("SpriteComponent"))
         menu.addAction(create_sprite)
         menu.exec_(QCursor.pos())
-    
+
+    def remove_component(self, comp):
+        self.obj.components.remove(self.obj.get_component(comp))
+        self.set_obj(self.obj)
+        self.parent.project.save()
+        self.parent.viewport.update_screen()
+
     def create_component(self, comp):
         if comp == "TransformComponent":
             self.obj.components.append(components.TransformComponent())
