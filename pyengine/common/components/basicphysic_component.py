62f0c9517aff82785135fa5c9bf64c54310cbb69
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-03-25 15:52:42 +0100

    Change default value of gravity in BasicPhysicComponent

--- pyengine/common/components/basicphysic_component.py
+++ pyengine/common/components/basicphysic_component.py
@@ -1,7 +1,7 @@
 class BasicPhysicComponent:
     def __init__(self):
         self.name = "BasicPhysicComponent"
-        self.gravity = 5
+        self.gravity = 125
 
     def to_dict(self):
         return {
@@ -12,5 +12,5 @@ class BasicPhysicComponent:
     @classmethod
     def from_dict(cls, values):
         comp = BasicPhysicComponent()
-        comp.gravity = values.get("gravity", 5)
+        comp.gravity = values.get("gravity", 125)
         return comp
